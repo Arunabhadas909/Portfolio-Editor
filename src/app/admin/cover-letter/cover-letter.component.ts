7a106ff8a930eb528c6f133e5d9aa51ec9d6a211
Arunabha Das <75629405+Arunabhadas909@users.noreply.github.com>
2025-06-23 12:12:03 +0530

    version - updating cover-letter.component.ts try-1

--- src/app/admin/cover-letter/cover-letter.component.ts
+++ src/app/admin/cover-letter/cover-letter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Service1Service } from 'src/app/service1.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { Service1Service } from 'src/app/service1.service';
   templateUrl: './cover-letter.component.html',
   styleUrls: ['./cover-letter.component.css']
 })
-export class CoverLetterComponent {
+export class CoverLetterComponent implements OnInit{
 
 
   constructor(private service:Service1Service){}
@@ -60,4 +60,37 @@ goBack()
   this.showDiv = 'div1';
 }
 
+
+
+   getCoverLetterText()
+{
+
+  this.service.getDataFromDatabase('/data').subscribe( async (info) =>
+    {
+      // const data = await info;
+      // const base64 : string| ArrayBuffer | null = await info.previewUrl;
+      // const mimeType = await info.mimeType;
+
+      // this.gotPreviewUrl = `data:${mimeType};base64,${base64}`;
+      this.textEntered= await info.coverLettertextEntered;
+
+
+      // console.log('goPreviewUrl' ,this.gotPreviewUrl);
+      // console.log('base64' ,base64);
+      console.log('Cover Letter Text',this.textEntered);
+      // console.log('data' , data);
+
+    })
+}
+
+
+
+  ngOnInit()
+  {
+
+    this.getCoverLetterText();
+  }
+
+
+  
 }
